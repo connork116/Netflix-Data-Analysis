5d104b751963522581b76f87bd30408fe15d1583
Connor King <cking6@zagmail.gonzaga.edu>
2022-07-20 18:51:01 -0700

    housekeeping

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('datasets/titles.csv')
+print(len(df))
 
 # Drpos NaNs for imdb_score column
 df = df.dropna(subset = ['imdb_score'])
+print(len(df))
+
+df = df.dropna(subset=['imdb_score'])
+print(len(df))
